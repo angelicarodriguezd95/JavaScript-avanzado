58919b5788255c74746cda37b4678cc663557666
angelicarodriguezd95 <118750418+angelicarodriguezd95@users.noreply.github.com>
2022-12-21 11:37:30 -0500

    Add files via upload

---
+++ TRABAJO15/index1.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const hostname= "127.0.0.1"
+const port=300;
+
+const server =http.createServer((req,res)=>{
+    res.statusCode=200;
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('ANGELICA RODRIGUEZ DOMINGUEZ')
+})
+
+server.listen(port, hostname, () => {
+    console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`)
+    })
